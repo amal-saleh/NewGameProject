794a59b21ca659f06a9c2ae577210fd6dd74d089
Amal Saleh <amal.saleh.mu@gmail.com>
2023-01-11 23:19:29 +0300

    adding html pages for footer contents

--- mainpage/views.py
+++ mainpage/views.py
@@ -6,3 +6,21 @@ from django.http import HttpResponse
 def index(request):
     #return HttpResponse('Hello World, muna test')
     return render(request, 'mainpage/index.html')
+
+
+def about(request):
+    #return HttpResponse('Hello World, muna test')
+    return render(request, 'mainpage/rules.html')
+
+def about(request):
+    #return HttpResponse('Hello World, muna test')
+    return render(request, 'mainpage/about.html')
+
+
+def about(request):
+    #return HttpResponse('Hello World, muna test')
+    return render(request, 'mainpage/questions.html')
+
+def about(request):
+    #return HttpResponse('Hello World, muna test')
+    return render(request, 'mainpage/achievements.html')
