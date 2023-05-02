07ae668efac355cc6f0e0c8125583160e42e4c3a
MunaMurad <munaunistd99@gmail.com>
2023-05-02 14:47:39 +0300

    adding DB and change the setting.py

--- gameproject/settings.py
+++ gameproject/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'gameproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ 'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '1999@6@10muna',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+ }
 }
 
 
